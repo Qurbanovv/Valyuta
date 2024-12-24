40c8902ac58a7b9f605b649430127bf133ca0135
Vahid Qurbanov <qurbanovvv2006@gmail.com>
2024-12-24 12:15:10 +0400

    finished code

--- valyuta.js
+++ valyuta.js
@@ -1,7 +1,34 @@
+// function clic(){
+//     let reqem=Number(document.getElementById("val_1").value)
+//     console.log(reqem)
+//     let a = reqem*1.7
+//     console.log(a)
+//     cixis.innerHTML=`${a}`
+// }
 function clic(){
     let reqem=Number(document.getElementById("val_1").value)
-    console.log(reqem)
-    let a = reqem*1.7
-    console.log(a)
-    cixis.innerHTML=`${a}`
+    let hansivalyuta = document.getElementById("hansivalyuta").value
+    let hansivalyutaya = document.getElementById("hansivalyutaya").value
+    // console.log(typeof(hansivalyuta))
+    if(hansivalyuta==="USD"&&hansivalyutaya==="AZE"){
+        let a =reqem*1.7
+        cixis.innerHTML=`${a}`
+        console.log("1")
+    }
+    else if(hansivalyuta==="AZE"&&hansivalyutaya==="USD"){
+        let a =reqem/1.7
+        cixis.innerHTML=`${a}`
+        console.log("2")
+    }
+    else if(hansivalyuta==="USD"&&hansivalyutaya==="USD"){
+        let a =reqem
+        cixis.innerHTML=`${a}`
+        // console.log("2")
+    }
+    else if(hansivalyuta==="AZE"&&hansivalyutaya==="AZE"){
+        let a =reqem
+        cixis.innerHTML=`${a}`
+        // console.log("2")
+    }
+    // else if (hansi valyuta = USd$$ hansivalyutaya=Aze) 1.7 ye bol
 }
